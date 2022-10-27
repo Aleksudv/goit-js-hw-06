058c44a68d5655fa204e370ec85dfc5f3d48eee5
Oleksandr Udovenko <107849168+Aleksudv@users.noreply.github.com>
2022-10-27 21:09:16 +0300

    variant 2

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,10 +1,24 @@
 
 
+// const inputEl = document.querySelector('#name-input');
+// const spanEl = document.querySelector('#name-output');
+
+// inputEl.addEventListener('input', (event) => {
+//     spanEl.textContent = event.currentTarget.value;
+// });
+// console.log(inputEl);
+// console.log(spanEl);
+
+
+
 const inputEl = document.querySelector('#name-input');
 const spanEl = document.querySelector('#name-output');
 
-inputEl.addEventListener('input', (event) => {
-    spanEl.textContent = event.currentTarget.value;
+inputEl.addEventListener('input', event => {
+    if (event.target.value === '') {
+        spanEl.textContent = 'Anonymous'
+        return;
+    }
+         spanEl.textContent=event.target.value;
 });
-console.log(inputEl);
-console.log(spanEl);
+
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,9 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
 
 const spanColorEl = document.querySelector('.color');
 const btn = document.querySelector('button');
@@ -10,3 +16,15 @@ const setBg = () => {
 
 btn.addEventListener("click", setBg);
 setBg();
+
+// const spanColorEl = document.querySelector('.color');
+// const btn = document.querySelector('button');
+
+// const setBg = () => {
+//   const getRandomHexColor = Math.floor(Math.random()*16777215).toString(16);
+//   document.body.style.backgroundColor = "#" + getRandomHexColor;
+//   spanColorEl.innerHTML = "#" + getRandomHexColor;
+// }
+
+// btn.addEventListener("click", setBg);
+// setBg();
